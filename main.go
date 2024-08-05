302be17ee402ab4b8fb6710cdc03a9a3957e8665
Eugene Osipenko <evgeniy.osipenko@betera.by>
2024-08-05 15:17:54 +0300

    task2

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	task1 "leetcode-go/tasks/easy/1_two_sum"
+	task2 "leetcode-go/tasks/medium/2_add_two_numbers"
 )
 
 func main() {
 	task1.New().Run("")
+	task2.New().Run("")
 }
